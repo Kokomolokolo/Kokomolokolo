3ace4cc6cb8aadbb37ddb4024096dae0f0488e7d
Kokomolokolo <157358834+Kokomolokolo@users.noreply.github.com>
2024-01-25 12:55:25 +0100

    Create Grundstruktur.py

---
+++ Grundstruktur.py
@@ -0,0 +1,12 @@
+"""#!/usr/bin/env python3
+### schebang line -  welcher Interpreter
+
+
+### Hier stehen Importe
+
+### Hier stehen Funktionen und Klassen
+
+###Hier steht das Hauptprogramm
+if __name__ == '__main__':
+    #Hier steht der Programmcode
+    print("Hallo Welt")"""
