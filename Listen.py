f069ca9f568a233b013432bbe5db795fa37385c0
Kokomolokolo <157358834+Kokomolokolo@users.noreply.github.com>
2024-01-25 13:02:44 +0100

    Create Listen.py

---
+++ Listen.py
@@ -0,0 +1,45 @@
+"""#!/usr/bin/env python3
+
+### Hier stehen Funktionen
+
+###Hier steht das Hauptprogramm
+if __name__ == '__main__':
+    #Hier setht der Programmcode
+    #Sequenzen: Strings, Bytesrings, Tupel, Listen
+    # Tupel
+    anrede = "Hey, wie gehts? "
+    frage = "Schönes Wetter heute."
+    zusammen = (anrede, frage)
+    print(zusammen)
+    """"""
+    anrede = 42
+    frage = 12
+    print(zusammen)
+    Geht Net
+    """"""
+    # Liste
+    list = ["apple", "banana", "cherry"]
+    # print(list) druckt alles
+    for Element in list:
+        print(Element)
+    
+    print()
+
+    liste_zahlen = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    
+    ausgabe_liste = []
+    
+    for element_zahl in liste_zahlen:
+        ausgabe_liste.append(element_zahl * 3)
+    for element_zahl in ausgabe_liste:     
+        print(element_zahl)
+    
+    print()
+
+    for element_zahl in ausgabe_liste:
+        element_zahl_modulo = element_zahl % 2
+        if element_zahl_modulo == 0:
+            print(element_zahl)
+    """
+    
+    
