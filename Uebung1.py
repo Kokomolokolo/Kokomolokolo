e2c37800281fc1864d34672821d2550fc073a8f6
Kokomolokolo <157358834+Kokomolokolo@users.noreply.github.com>
2024-01-25 12:57:08 +0100

    Create Uebung1.py

---
+++ Uebung1.py
@@ -0,0 +1,70 @@
+"""#!/usr/bin/env python3
+
+### Hier stehen Funktionen
+
+### Hier steht das Hauptprogramm
+if __name__ == '__main__':
+    # Hier setht der Programmcode
+    # Einfache Zuweisung
+    x = 5
+    print(x)
+    
+    # Mehrfache Zuweisung
+    x, y, z = 10, 15, 20
+    print(x)
+    print(y)
+    print(z)
+  
+    # Datentyp bestimmen
+    print(type("Thomas"))
+    print(type(5))
+    print(type(5.5))
+    # Geht auch mit Variablen
+    a = 3
+    print(type(a))
+    
+    # Der Modulo Operator %
+    11 % 2 # 1, dann 2 steckt 5 mal in der 11, Rest 1
+    # Ganzzahlige Division mit //
+    3 // 2 # = 1
+    10 // 3 # 3
+  
+    # Strings
+    print("Das ist die 1. Zeile\nund das die 2. Zeile")
+
+    # Zugriffe auf Teile einer Zeile
+    zeile = "Alle meine Entchen"
+    print(zeile)
+    # Teil a bis b
+    print(zeile[3:7])
+    # Teil Anfang bis b
+    print(zeile[:7])
+    # Teil a bis Ende
+    print(zeile[3:])
+    # Teile von Hinten
+    print(zeile[-1])
+    print(zeile[-5:-2])
+    
+    # Boolean
+    print("heute" == "morgen")
+    print(11<12)
+    b = 123 < 23
+    # if b == False:
+        # print("Ist kleiner")
+    """
+    """
+    <   Kleiner als
+    >   Größer Als
+    <=  Kleiner gleich
+    >=  Größer gleich
+    ==  Gleich
+    !=  Ungleich
+    """
+    """
+    print(b)
+    
+    # Typunwandlungen
+    t = int("2") # aus dem str "2" wind die int-Zahl
+    print(t)
+    print(t+t)"""
+
